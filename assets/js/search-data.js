2f8cf1353467047b81f4d0ecb45f6dd78a49bf00
jordieskarajew <jordie.skarajew@gmail.com>
2026-03-20 10:01:30 +0000

    Deploying to gh-pages from @ jordieskarajew/jordieskarajew.github.io@f88738a90719eef6dc096cccbedf946ec4aa4a06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "",
           section: "Navigation",
           handler: () => {
