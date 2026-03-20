c11cf5e5f66caf8fe8f3ee1898abfb077af58419
jordieskarajew <jordie.skarajew@gmail.com>
2026-03-20 09:37:24 +0000

    Deploying to gh-pages from @ jordieskarajew/jordieskarajew.github.io@6a7c92034ee619d37ad0ccc3ee63cfa21079296e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-the-riemannian-geometry-of-financial-correlation",
+          title: 'The Riemannian Geometry of Financial Correlation',
+          description: "Research paper on affine-invariant geometry of financial correlation matrices for systemic stress detection.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
