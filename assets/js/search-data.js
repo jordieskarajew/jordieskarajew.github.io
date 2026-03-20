b289c0371c79332022b7a216d780d04b99165293
jordieskarajew <jordie.skarajew@gmail.com>
2026-03-20 10:08:39 +0000

    Deploying to gh-pages from @ jordieskarajew/jordieskarajew.github.io@35653a7698fc40e85cfcd5994ecfece0c7837dd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,16 +456,6 @@ ninja.data = [{
           description: "Research paper on affine-invariant geometry of financial correlation matrices for systemic stress detection.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
